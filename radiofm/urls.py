dbd4ceabb304c960272e8cc67bbd03dec9dfa008
LucasOliver <78925489+spinho010@users.noreply.github.com>
2022-01-01 16:58:39 -0300

    paulo contruções

--- radiofm/urls.py
+++ radiofm/urls.py
@@ -15,9 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from users.views import PauloContruções
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include("allauth.urls")),
     path("", include("pages.urls", namespace="pages")),
+    path("divulgacao1/", PauloContruções.as_view(), name="divulgação"),
 ]
--- users/views.py
+++ users/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib.auth.models import User
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.views.generic.list import ListView
+from django.http import HttpResponse
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.urls import reverse_lazy
+from django.views.generic import TemplateView
+from django.template.loader import get_template
 
 # Create your views here.
+
+
+#TemplateView
+
+class PauloContruções(TemplateView):
+    template_name = 'paulo.html'
+
+
